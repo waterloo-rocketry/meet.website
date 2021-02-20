f29893c71e1738a231407e3c72634ba52eba931f
Wendi <wendi.yu@yahoo.ca>
2021-02-19 22:09:36 -0700

    Allow customized description

--- getQueryString.js
+++ getQueryString.js
@@ -11,4 +11,18 @@ function getQueryVariable(variable) {
   });
 
   return value;
-  }
+}
+
+function prettify(line) {
+    // separate underscore-delineated words
+    const words = line.split('_').map(word => {
+        // place spaces between letters and digits
+        word = word.replace(/([a-zA-Z])(\d)/g, '$1 $2');
+        word = word.replace(/(\d)([a-zA-Z])/g, '$1 $2');
+        // capitalize first letter
+        return word[0].toUpperCase() + word.substring(1);
+    })
+
+    console.log(words.join(' '));
+    return words.join(' ');
+}
