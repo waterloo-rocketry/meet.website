7c03ff1c506187501164aedd2b947d1e8d8aa8a0
Wendi <wendi.yu@yahoo.ca>
2021-03-01 16:19:20 -0700

    Serve from 404 page

--- prettify.js
+++ prettify.js
@@ -1,18 +1,3 @@
-function getQueryVariable(variable) {
-  const query = window.location.search.substring(1);
-  const queryList = query.split('&');
-  let value = undefined;
-
-  queryList.forEach(pair => {
-    const pairVals = pair.split('=');
-    if (decodeURIComponent(pairVals[0]) === variable) {
-      value = decodeURIComponent(pairVals[1]);
-    }
-  });
-
-  return value;
-}
-
 function prettify(line) {
     // separate underscore-delineated words
     const words = line.split('_').map(word => {
